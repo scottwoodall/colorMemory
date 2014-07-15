73b50ddab620ad703b027fd1192624ab903e9005
Scott Woodall <scott.woodall@gmail.com>
2014-07-15 06:37:41 -0400

    added font-awesome and sprinkled in some icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,13 +12,13 @@ var NavBar = React.createClass({
                 <div className="container-fluid">
                     <div className="navbar-header">
                         <a className="navbar-brand">
-                            Color Memory
+                            <i className="fa fa-mortar-board"></i> Color Memory
                         </a>
                     </div>
                     <ul className="nav navbar-nav navbar-right">
                         <li>
                             <a href="https://github.com/scottwoodall/colorMemory">
-                                Github
+                                <i className="fa fa-github"></i> Github
                             </a>
                         </li>
                     </ul>
